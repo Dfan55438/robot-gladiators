e90b0acf3424ed0f12d85d0efbe247d9d9f161cb
Wei <Dfan55438>
2020-12-28 23:30:18 -0800

    robot glad update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Jacascript is running!!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+console.log("Player " + playerName + " has entered the fight!");
+//console.log used during testing so that window.alert doesnt affect end users
+console.log
+
+function fight()
+    {
+        window.alert("The fight has begun!");
+    }
+
+    
+    //fight();
